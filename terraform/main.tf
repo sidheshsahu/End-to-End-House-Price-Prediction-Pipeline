3f5eecca98dc3dda516b1fb9470cfe7699d897d4
sidheshsahu <sidhesh.sahu@somaiya.edu>
2026-03-12 13:51:03 +0530

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"
+    source_address_prefix      = "10.89.23.232"
     destination_address_prefix = "*"
   }
 }

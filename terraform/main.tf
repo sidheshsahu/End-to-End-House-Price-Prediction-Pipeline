fa233c7472a2c649250b20e3f3574546d76be75b
sidheshsahu <sidhesh.sahu@somaiya.edu>
2026-03-13 14:16:59 +0530

    Updated jenkinsfile and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "YOUR_IP/32"
+    source_address_prefix      = "10.85.62.232"
     destination_address_prefix = "*"
   }
 

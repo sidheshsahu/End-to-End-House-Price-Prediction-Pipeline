3814dba1876fed76fe804dd2ea5dd4b608a05ead
sidheshsahu <sidhesh.sahu@somaiya.edu>
2026-03-13 13:44:27 +0530

    Updated jenkinsfile and main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "5000"
-    source_address_prefix      = "10.89.23.232"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
@@ -78,12 +78,16 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location                        = azurerm_resource_group.rg.location
   size                            = "Standard_B1s"
   admin_username                  = "azureuser"
-  admin_password                  = "DevOps@123456"
-  disable_password_authentication = false
+  disable_password_authentication = true
 
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = "AAAAB3NzaC1yc2EAAAADAQABAAACAQCwSRNGxGloDSW4q+NpVdWj+9Dzm9jqd1jqhzbXtTlXyCvbXevl+LXYRUO2efftH5z96y95WE6GbhRQvxrKsxxu/wHwFOTT+GZdf0ts5qcf4tZ5CV4vlC5ve8LTuBvHmm10AYfjblMY2dyQ9yiovhr4VchOZ8pUDWmrcYxiOvN1ghlC6q0oZjcMyZ9tv848Ybm1Leu/mQ89NRy9KLIaMdSx34t118tUx2HEAzohpXzVZ/STDFTu6R8frfFItiTf8Vb3LqZBo6AZPAjvxfDamP0AsSPaY8O5wx6pGLy8gIhEYi+B45zzwFeTeHUPKU01Hrss/fU63VDf+uJ2xil0AQgZrA9VBqWyyGvKvqkiw05JafbpjNsdhtBRPKA5btARnmDzZMeR/EcExgOFiHeBiPAliyL69Z6xtTx0PeoGJrXcECcdIvy81qXRWoGuNf4VaUqQqAvh+uQ5yG8PiTL6yJ4CjhfqEo6vYvoaT1qoYKMvJ38D93AH72bUo+l3Pyvwf/7qLOiO5f9Gpm68OC0ndPjQ+bghpGsw+YD/KOyr+gMeYCTqPH5U3OeABpWFxzM/8f7jRedhPfBTtI/fb4g15uIayVW1vQ/m6drBl2OqYi0u+bYzIpnHO+S6vjudvHXmX0v6Q/qosTvERdCEmVUqZvK1Mf/xXuvIpg3ai2rq2VX0uQ"
+  }
 
   os_disk {
     caching              = "ReadWrite"
